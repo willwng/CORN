[Bug fix] Fix incorrect calculation of r_ij vectors

energyminimization/energies/stretch_nonlinear.py
@@ -38,8 +38,13 @@ def get_nl_stretch_jacobian(u_matrix: np.ndarray, stretch_mod: float, r_matrix: 
     return gradient
 
 
-def get_nl_stretch_energy(u_matrix: np.ndarray, stretch_mod: float, r_matrix: np.ndarray, length_matrix: np.ndarray,
-                          active_bond_indices: np.ndarray) -> float:
+def get_nonlinear_stretch_energy(
+        stretch_mod: float,
+        u_matrix: np.ndarray,
+        r_matrix: np.ndarray,
+        active_bond_indices: np.ndarray,
+        active_bond_lengths: np.ndarray
+) -> float:
     d, _ = get_bond_lengths(u_matrix=u_matrix, r_matrix=r_matrix, length_matrix=length_matrix,
                             active_bond_indices=active_bond_indices)
     energy = 0.5 * stretch_mod * np.square(d)

energyminimization/matrix_helper.py
@@ -30,6 +30,7 @@ def create_pos_matrix(lattice: AbstractLattice) -> np.ndarray:
     # Use pre-computed result
     if lattice in lattice_to_pos:
         return lattice_to_pos[lattice]
+    # Create new matrix
     nodes = lattice.get_nodes()
     num_nodes = len(nodes)
     pos_matrix = np.zeros((num_nodes, 2))
@@ -38,6 +39,7 @@ def create_pos_matrix(lattice: AbstractLattice) -> np.ndarray:
         n_id = node.get_id()
         pos_matrix[n_id][0] = n_pos[0]
         pos_matrix[n_id][1] = n_pos[1]
+    # Store matrix
     lattice_to_pos[lattice] = pos_matrix
     return pos_matrix
 
@@ -128,7 +130,10 @@ def create_boundary_matrix(
 
 
 def create_r_matrix(
-        pos_vector: np.ndarray, edge_matrix: np.ndarray, active_bond_indices: np.ndarray, normalize: bool
+        pos_vector: np.ndarray,
+        active_bond_indices: np.ndarray,
+        lattice: AbstractLattice,
+        normalize: bool
 ) -> np.ndarray:
     """
     Returns the matrix containing vectors between each node.
@@ -138,11 +143,9 @@ def create_r_matrix(
     :param pos_vector: Position of each node (will be reshaped) in order of
         [x_1, y_1, x_2, ...]
     :type pos_vector: Must contain 2n elements
-    :param edge_matrix: matrix used to update positions of the edge nodes
-        (so edge bonds are not overly long)
-    :type edge_matrix: Shape (n_bonds, 2)
     :param active_bond_indices: List containing indices of [i, j]
     :type active_bond_indices: Shape (# bonds, 2) matrix
+    :param lattice: Lattice object
     :param normalize: Whether to normalize the vectors to unit vectors
     :type normalize: bool
     :return: Shape (n, n, 2) matrix
@@ -150,59 +153,22 @@ def create_r_matrix(
     pos_matrix = pos_vector.reshape((-1, 2))
     # Change in x from node to node
     i, j = active_bond_indices[:, 0], active_bond_indices[:, 1]
-    r_matrix = pos_matrix[j, :] - pos_matrix[i, :]
-    r_matrix -= edge_matrix  # Subtract the x values for edge bonds
+
+    # Periodic boundary conditions require correction
+    hor_pbc, top_pbc, idx = active_bond_indices[:, 2], active_bond_indices[:, 3], active_bond_indices[:, 4]
+
+    idx_hor = idx[np.where(hor_pbc == 1)]
+    idx_top = idx[np.where(top_pbc == 1)]
+    correction = np.zeros((len(active_bond_indices), 2))
+    correction[idx_hor, 0] = lattice.get_length()
+    correction[idx_top, 1] = lattice.get_height() + lattice.height_increment
+
+    r_matrix = pos_matrix[j, :] - pos_matrix[i, :] + correction
     if normalize:
         norm_factor = np.linalg.norm(r_matrix, axis=1, keepdims=True)
         r_matrix = r_matrix / norm_factor
+
     return r_matrix
-
-
-def create_length_matrix(
-        pos_vector: np.ndarray, edge_matrix: np.ndarray, active_bond_indices: np.ndarray
-) -> np.ndarray:
-    """
-    Matrix representing the rest lengths of each bond (similar to r_matrix)
-    """
-    pos_matrix = pos_vector.reshape((-1, 2))
-    i, j = active_bond_indices[:, 0], active_bond_indices[:, 1]
-    r_matrix = pos_matrix[j, :] - pos_matrix[i, :]
-    r_matrix -= edge_matrix
-    length_matrix = np.linalg.norm(r_matrix, axis=1, keepdims=True)
-    return length_matrix.flatten()
-
-
-def create_edge_matrix(lattice: AbstractLattice, init_pos: np.ndarray, active_bond_indices: np.ndarray) -> np.ndarray:
-    """
-    create_edge_matrix is used to create a matrix that ensures the edge bonds are not overly long. Used
-    by subtracting this matrix from r_matrix
-
-    :param lattice: lattice object
-    :param init_pos: initial position matrix of the nodes
-    :type: Shape (# active_bonds, 2)
-    :param active_bond_indices: List containing indices of [i, j]
-    :type active_bond_indices: Shape (# bonds, 2) matrix
-    """
-    n = len(active_bond_indices)
-    edge_matrix = np.zeros((n, 2))
-
-    i, j = active_bond_indices[:, 0], active_bond_indices[:, 1]
-    hor_pbc, top_pbc = active_bond_indices[:, 2], active_bond_indices[:, 3]
-
-    # # Horizontal periodic boundary conditions
-    # x_i, x_j = init_pos[i, 0], init_pos[j, 0]
-    # e1 = np.where((hor_pbc == 1) & (x_i > x_j))
-    # e2 = np.where((hor_pbc == 1) & (x_i <= x_j))
-    # assert e2[0].size == 0  # x position of node_i should always be greater than node_j
-    # edge_matrix[e1, 0] = -lattice.get_length()
-    #
-    # # Vertical periodic boundary conditions
-    # y_i, y_j = init_pos[i, 1], init_pos[j, 1]
-    # e1 = np.where((top_pbc == 1) & (y_i > y_j))
-    # e2 = np.where((top_pbc == 1) & (y_i <= y_j))
-    # assert e2[0].size == 0  # y position of node_i should always be greater than node_j
-    # edge_matrix[e1, 1] = -lattice.get_height() - lattice.height_increment
-    return edge_matrix
 
 
 def create_correction_matrix(lattice: AbstractLattice, init_pos: np.ndarray,

energyminimization/minimize.py
@@ -77,34 +77,29 @@ def minimize(
     """
     start_time = time.time()
 
-    bonds = lattice.get_active_bonds()
-    pi_bonds = lattice.get_active_pi_bonds()
-
-    active_bonds = [b for b in bonds if b.exists()]
-    active_pi_bonds = [p for p in pi_bonds if p.exists()]
-
     # Map from bond object to index (in active_bond_indices and r_matrix)
     bond_to_idx = {}
 
-    # active_bond_indices[i] contains: [node_1 id, node_2 id, horizontal PBC, top PBC, index for r_matrix]
-    active_bond_indices = np.zeros((len(active_bonds), 5), dtype=np.int32)
-    for i, bond in enumerate(active_bonds):
-        if bond.exists():
-            active_bond_indices[i][0] = bond.get_node1().get_id()
-            active_bond_indices[i][1] = bond.get_node2().get_id()
-            active_bond_indices[i][2] = bond.is_hor_pbc()
-            active_bond_indices[i][3] = bond.is_top_pbc()
-            active_bond_indices[i][4] = i
-            bond_to_idx[bond] = i
-
+    # all_bond_indices[i] contains: [node_1 id, node_2 id, horizontal PBC, top PBC, index for r_matrix]
     all_bond_indices = np.zeros((len(lattice.get_bonds()), 5), dtype=np.int32)
+    i_active = 0
     for i, bond in enumerate(lattice.get_bonds()):
         all_bond_indices[i][0] = bond.get_node1().get_id()
         all_bond_indices[i][1] = bond.get_node2().get_id()
         all_bond_indices[i][2] = bond.is_hor_pbc()
         all_bond_indices[i][3] = bond.is_top_pbc()
+        if bond.exists():
+            all_bond_indices[i][4] = i_active
+            bond_to_idx[bond] = i_active
+            i_active += 1
+        else:
+            all_bond_indices[i][4] = -1
+
+    # Same as all_bond_indices but only for active bonds
+    active_bond_indices = all_bond_indices[all_bond_indices[:, 4] != -1]
 
     # active_pi_indices[i] contains: [vertex id, edge 1 id, edge 2 id, bond 1 id, sign for r_matrix[bond id]]
+    active_pi_bonds = lattice.get_active_pi_bonds()
     active_pi_indices = np.zeros((len(active_pi_bonds), 5), dtype=np.int32)
     for i, pi_bond in enumerate(active_pi_bonds):
         if pi_bond.exists():
@@ -116,17 +111,19 @@ def minimize(
 
     # Initial position of nodes, initial unit vectors for bonds, initial energy
     init_pos = pos.create_pos_matrix(lattice)
-    edge_matrix = pos.create_edge_matrix(lattice, init_pos, active_bond_indices)
-    r_matrix = pos.create_r_matrix(init_pos, edge_matrix, active_bond_indices, True)
     correction_matrix = pos.create_correction_matrix(lattice=lattice, init_pos=init_pos,
                                                      trans_matrix=trans_matrix, all_bond_indices=all_bond_indices)
+    r_matrix = pos.create_r_matrix(pos_vector=init_pos, active_bond_indices=active_bond_indices, lattice=lattice,
+                                   normalize=True)
+    length_matrix = pos.create_r_matrix(pos_vector=init_pos, active_bond_indices=active_bond_indices, lattice=lattice,
+                                        normalize=False)
 
-    length_matrix = pos.create_length_matrix(init_pos, edge_matrix, active_bond_indices)
+    # length_matrix = pos.create_length_matrix(init_pos, edge_matrix, active_bond_indices)
 
     # Solve for the final relaxed position
     solve_params = solver.SolveParameters(lattice=lattice, init_pos=init_pos, sheared_pos=sheared_pos,
                                           init_guess=init_guess, r_matrix=r_matrix, correction_matrix=correction_matrix,
-                                          length_matrix=length_matrix, active_bond_indices=active_bond_indices,
+                                          length_matrix=None, active_bond_indices=active_bond_indices,
                                           active_pi_indices=active_pi_indices, stretch_mod=stretch_mod,
                                           bend_mod=bend_mod, tran_mod=tran_mod, tolerance=tolerance)
     solve_result = solver.solve(params=solve_params, minimization_type=minimization_method,